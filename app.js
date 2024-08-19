1a832f5d557f6a5d63af0fc94beeb39680b3ca37
Martin De Lojo <martinmdl42@gmail.com>
2024-08-19 14:42:38 -0300

    read curl calls

--- app.js
+++ app.js
@@ -5,28 +5,16 @@ const api = express();
 const HOST = 'localhost';
 const PORT = 3000;
 
-api.use(express.urlencoded({ extended: true }));
-
-api.get('/', (req, res) => {
-    res.send('HOMEE');
-});
-
-// const fullUrl = req.originalUrl;
-// const fullUrl = req.url;
-// const amazonUrl = req.query.productUrl;
-// const insideParams = req.params;
-
-api.post('/productUrl', (req, res) => {
-    const url = req.url;
-    // const productDescription = textCleaner(url);
-    // res.send(productDescription);
-    if(url) {
-        res.send(`URL RECIBIDA: ${url}`);
+api.post('/', (req, res) => {
+    const amazonUrl = req.query.productUrl;
+    const productDescription = textCleaner(amazonUrl);
+    res.send(productDescription);
+    if(amazonUrl) {
+        res.send(`URL RECIBIDA: ${amazonUrl}`);
     } else {
         res.status(400).send('URL no encontrada');
     }
 });
-
 
 api.listen(PORT, () => console.log(`API running at ${HOST}:${PORT}`));
 
@@ -36,6 +24,3 @@ api.listen(PORT, () => console.log(`API running at ${HOST}:${PORT}`));
 // chmod +x three_requests.sh
 // ./simulateRequests.sh localhost 3000 productUrl 6
 // ./three_requests.sh localhost 3000 productUrl 6
-
-
-
