87f16c01f820e497b2317d483ba635d597707cee
Martin De Lojo <martinmdl42@gmail.com>
2024-08-19 03:54:45 -0300

    comment script annotations

--- app.js
+++ app.js
@@ -31,19 +31,11 @@ api.post('/productUrl', (req, res) => {
 api.listen(PORT, () => console.log(`API running at ${HOST}:${PORT}`));
 
 /******* SCRIPT *******/
-
-// ./simulateRequests.sh <server_address> <port_number> <param_name> <sleep_time>
+// ./simulateRequests.sh <SERVER> <PORT> <PARAM> <SLEEP_TIME>
 // chmod +x simulateRequests.sh
 // chmod +x three_requests.sh
 // ./simulateRequests.sh localhost 3000 productUrl 6
 // ./three_requests.sh localhost 3000 productUrl 6
-
-// recibo una request:
-// curl -X POST localhost:3000/productUrl=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fproduct%2FB00SMBFZNG"
-
-// curl: (3) URL using bad/illegal format or missing URL
-// sleep: missing operand
-// Try 'sleep --help' for more information
 
 
 
